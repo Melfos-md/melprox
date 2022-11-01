31e26934f01f6e2f81c51d403654e5efae62d8ba
Melfos-md <romain.montoriol@gmail.com>
2022-11-01 18:13:58 +0100

    première version fonctionnelle

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
-fn main() {
-    println!("Hello, world!");
+//! On veut accéder à un serveur web exposé uniquement en 127.0.0.1
+//! L'attaquant sera le client du proxy et la cible le serveur.
+//! 
+//! Attaquant (client) <--------------------> Serveur (proxy) <-----------------------> localhost (cible)
+//!                      Connection entrante                      Connection sortante
+//!                         = Listener                                  = out_conn
+//!                    -----client_read----->                     -----server_write----->
+//!                    <------client_write---                     <----server_read------
+
+
+use tokio::{net::{TcpListener, TcpStream}, try_join};
+
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let listener = TcpListener::bind("0.0.0.0:8080").await?;
+
+    // Accepte les connections entrantes et crée un thread
+    while let Ok((client_conn,_)) = listener.accept().await {
+        tokio::spawn(async move {
+            println!("Connection établie");
+            process_socket(client_conn).await;
+        });
+    }
+
+    Ok(())
+}
+
+/// Crée une connection avec la cible (127.0.0.1:port)
+/// Split la connection entrante en ReadHalf et WriteHalf
+/// Copie le flux de la connection entrant vers la connection sortante
+async fn process_socket(mut client_conn: TcpStream) -> Result<(), Box<dyn std::error::Error>>{
+    let mut out_conn = TcpStream::connect("127.0.0.1:7331").await?;
+
+    let (mut client_read, mut client_write) = client_conn.split();
+    let (mut server_read, mut server_write) = out_conn.split();
+
+    let in_conn = tokio::io::copy(&mut client_read, &mut server_write);
+    let out_conn = tokio::io::copy(&mut server_read, &mut client_write);
+
+    try_join!(in_conn, out_conn)?;
+
+    Ok(())
 }
--- test/dummy_server/src/main.rs
+++ test/dummy_server/src/main.rs
@@ -2,7 +2,7 @@ use actix_web::{App, HttpResponse, HttpServer, Responder, get};
 
 #[get("/")]
 async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello, I am a dummy server on 127.0.0.1:8080\n")
+    HttpResponse::Ok().body("Hello, I am a dummy server on 127.0.0.1:7331\n")
 }
 
 #[actix_web::main]
@@ -11,7 +11,7 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .service(hello)
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("127.0.0.1", 7331))?
     .run()
     .await
 }
